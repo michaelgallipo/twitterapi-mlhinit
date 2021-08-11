45eb13475e1ccbf8a631a08b749664eec54bbac1
michael <michael.gallipo@gmail.com>
2021-08-11 15:35:14 -0400

    Added cors package to backend

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const routes = require('./routes/index');
+const cors = require('cors');
 // const bodyParser = require('body-parser');
 const app = express();
 
 // app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', routes);
+app.use(cors());
 
 module.exports = app;
