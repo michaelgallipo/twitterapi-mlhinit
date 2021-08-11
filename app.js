5d16775ca9635051f4d8a68bfeb0678a4132197c
michael <michael.gallipo@gmail.com>
2021-08-11 17:15:37 -0400

    Fixing CORS error

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const routes = require('./routes/index');
 const cors = require('cors');
 // const bodyParser = require('body-parser');
 const app = express();
-
+app.use(cors());
 // app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', routes);
-app.use(cors());
+
 
 module.exports = app;
